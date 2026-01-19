dabee0fb688cf39c662de99d1d6890d94e810edf
EricCR85 <ecr.ceo@gmail.com>
2026-01-19 11:44:54 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
-
+import Landing from './components/Landing';
+import Highlights from './components/Highlights';
+// import Highlight from './components/ui/Highligh';
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
