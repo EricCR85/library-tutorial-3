d2199097fc451f4fdecef169faa660d9f300830b
EricCR85 <ecr.ceo@gmail.com>
2026-01-19 11:53:21 -0500

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,11 @@
+
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faTimes,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
 import LibraryLogo from "../assets/Library.svg";
 
 const Nav = () => {
@@ -7,45 +13,53 @@ const Nav = () => {
     <nav>
       <div className="nav__container">
         <a href="/">
-          <img src={LibraryLogo} alt="" className="logo" />
+          <img src={LibraryLogo} alt="Library Logo" className="logo" />
         </a>
+
         <ul className="nav__links">
           <li className="nav__list">
             <a href="/" className="nav__link">
               Home
             </a>
           </li>
+
           <li className="nav__list">
-            <a href="/" className="nav__link">
+            <a href="/books" className="nav__link">
               Books
             </a>
           </li>
+
           <button className="btn__menu">
-            <FontAwesomeIcon icon="bars" />
+            <FontAwesomeIcon icon={faBars} />
           </button>
+
           <li className="nav__icon">
             <a href="/cart" className="nav__link">
-              <FontAwesomeIcon icon="shoping-cart" />
+              <FontAwesomeIcon icon={faShoppingCart} />
             </a>
             <span className="cart__length">2</span>
           </li>
         </ul>
-        <div className="menu__backdroop">
+
+        <div className="menu__backdrop">
           <button className="btn__menu btn__menu--close">
-            <FontAwesomeIcon icon="times" />
+            <FontAwesomeIcon icon={faTimes} />
           </button>
-          <ul className="Menu__links">
-            <li className="nemu__list">
+
+          <ul className="menu__links">
+            <li className="menu__list">
               <a href="/" className="menu__link">
                 Home
               </a>
             </li>
-            <li className="nemu__list">
+
+            <li className="menu__list">
               <a href="/books" className="menu__link">
                 Books
               </a>
             </li>
-            <li className="nemu__list">
+
+            <li className="menu__list">
               <a href="/cart" className="menu__link">
                 Cart
               </a>
