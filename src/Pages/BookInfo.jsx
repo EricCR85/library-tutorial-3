ebc80a13e2a4e116a2be297bbbdaa8fb020414e1
EricCR85 <ecr.ceo@gmail.com>
2026-01-22 13:41:46 -0500

    update

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -6,9 +6,7 @@ const BookInfo = ({ books }) => {
       <main id="books__main">
         <div className="books__container">
           <div className="row">
-            <div className="book__selected--top">
-                
-            </div>
+            <div className="book__selected--top"></div>
           </div>
         </div>
       </main>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,9 +11,9 @@ const Landing = () => {
             <h2>
               Find your dream book with <span className="Blue">Library</span>
             </h2>
-            <a href="#features">
+            <Link to="#features">
               <button className="btn">Browse books</button>
-            </a>
+            </Link>
           </div>
           <figure className="header__img--wrapper">
             <img src={UndrawBooks} alt="" />
