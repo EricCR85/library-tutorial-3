1a414dc86373d94c4114d53ee764558a29346197
EricCR85 <ecr.ceo@gmail.com>
2026-01-21 23:24:17 -0500

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Landing from "./components/Landing";
 import Highlights from "./components/Highlights";
 import Featured from "./components/Featured";
 import Explore from "./components/Explore";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Books  from "./Pages/Books";
 import { books } from "./data"
@@ -19,9 +19,11 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
+        <Routes>
         <Route path="/" exact component={Home} />
         <Route path="/books" render={() => <Books books={books} /> } />
         <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        </Routes>
         <Footer />
         
       </div>
